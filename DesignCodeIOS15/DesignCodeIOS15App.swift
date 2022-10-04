b0169b4a1a415c4136603436b8f2322599aefa1b
Maksym Donets <timnirton7@gmail.com>
2022-10-04 13:54:40 -0400

    Initial Commit

---
+++ DesignCodeIOS15/DesignCodeIOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeIOS15App.swift
+//  DesignCodeIOS15
+//
+//  Created by Max Donets on 04.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeIOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
