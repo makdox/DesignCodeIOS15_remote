c71bda24f9d31b2f305289762d5c93acc7232b8a
Maksym Donets <timnirton7@gmail.com>
2022-10-05 12:33:49 -0400

    create

---
+++ DesignCodeIOS15/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  DesignCodeIOS15
+//
+//  Created by Max Donets on 05.10.2022.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("sdf")
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
