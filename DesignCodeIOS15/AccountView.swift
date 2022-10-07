68f6faa255ecce757d1e9bd649a0ed09c4d5d7fd
Maksym Donets <timnirton7@gmail.com>
2022-10-07 15:45:34 -0400

    Navigation Link and URL

--- DesignCodeIOS15/AccountView.swift
+++ DesignCodeIOS15/AccountView.swift
@@ -37,13 +37,41 @@ struct AccountView: View {
                 }
                 .frame(maxWidth: .infinity)
                 .padding()
+                
                 Section {
-                    Label("Settings", systemImage: "gear")
-                    Label("Billing", systemImage: "creditcard")
-                    Label("Help", systemImage: "questionmark")
-                        .imageScale(.small)
+                    NavigationLink(destination: ContentView()) {
+                        Label("Settings", systemImage: "gear")
+                    }
+                    NavigationLink { Text("Billing") } label: {
+                        Label("Billing", systemImage: "creditcard")
+                    }
+                    NavigationLink { ContentView() } label: {
+                        Label("Help", systemImage: "questionmark")
+                    }
                 }
+                .accentColor(.primary)
                 .listRowSeparatorTint(.blue)
+                .listRowSeparator(.hidden)
+                
+                Section {
+                    Link(destination: URL(string: "https//appple.com")!) {
+                        HStack {
+                            Label("Website", systemImage: "house")
+                            Spacer()
+                            Image(systemName: "link")
+                                .foregroundColor(.secondary)
+                        }
+                    }
+                    Link(destination: URL(string: "https//youtube.com")!) {
+                        HStack {
+                            Label("Youtube", systemImage: "tv")
+                            Spacer()
+                            Image(systemName: "link")
+                                .foregroundColor(.secondary)
+                        }
+                    }
+                }
+                .accentColor(.primary)
                 .listRowSeparator(.hidden)
             }
             .listStyle(.insetGrouped)
@@ -55,5 +83,6 @@ struct AccountView: View {
 struct AccountView_Previews: PreviewProvider {
     static var previews: some View {
         AccountView()
+            .preferredColorScheme(.light)
     }
 }
