0de1936b69adc6691e7a987a2b0db0ad68846cc7
Maksym Donets <timnirton7@gmail.com>
2022-10-08 16:44:44 -0400

    SVG to Path in Canvas

---
+++ DesignCodeIOS15/Blob 1.swift
@@ -0,0 +1,24 @@
+//
+//  Blob 1.swift
+//  DesignCodeIOS15
+//
+//  Created by Max Donets on 08.10.2022.
+//
+
+import SwiftUI
+
+struct Blob_1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Blob_1_Previews: PreviewProvider {
+    static var previews: some View {
+        Blob_1()
+    }
+}
+
+struct BlobShape: Shape {
+    
+}
